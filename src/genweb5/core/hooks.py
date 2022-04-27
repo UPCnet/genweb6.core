9d059bc01b9c3cde7a12e25987f4513e62ed3a80
Iago López <ilopezsmx2011@gmail.com>
2022-04-27 16:46:58 +0200

    Migrate hooks.py

---
+++ src/genweb5/core/hooks.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import safe_unicode
+
+from cgi import escape
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from plone import api
+
+import socket
+
+
+def addedPermissionsPloneSiteRoot(content, event):
+    portal = api.portal.get()
+    # sender_email = portal.getProperty('email_from_address')
+    # sender_name = portal.getProperty('email_from_name').encode('utf-8')
+    email_charset = portal.getProperty('email_charset')
+    # fromMsg = sender_name + ' ' + '<' + sender_email + '>'
+    fromMsg = 'gestio.genweb@upc.edu'
+
+    serverid = socket.gethostname()
+    mountpoint = '/'.join(content.getPhysicalPath())
+    plone = content.absolute_url()
+
+    # TODO Enviar correo para abrir tiquet
+    context = aq_inner(content)
+    mailhost = getToolByName(context, 'MailHost')
+    msg = MIMEMultipart()
+    msg['From'] = fromMsg
+    msg['To'] = 'mailtoticket.areatic@upc.edu'
+    msg['Subject'] = escape(safe_unicode('Recatalogar el genweb ' + plone))
+    msg['charset'] = email_charset
+
+    message = "Màquina: " + serverid + "\nMontpoint: " + mountpoint + "\nURL: " + plone
+
+    msg.attach(MIMEText(message, 'plain', email_charset))
+    mailhost.send(msg)
