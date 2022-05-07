fd19a9c931a406377f45c2217dd5219cb3049620
Iago López <ilopezsmx2011@gmail.com>
2022-05-07 15:51:23 +0200

    Actualizar locales/update.sh

--- src/genweb5/core/locales/update.sh
+++ src/genweb5/core/locales/update.sh
@@ -1,15 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
-cd ../../../../../
-./bin/i18ndude rebuild-pot \
-  --pot src/genweb.core/genweb/core/locales/genweb.pot \
-  --create genweb \
-  src/genweb.banners/ src/genweb.controlpanel src/genweb.core \
-  src/genweb.jsonify  src/genweb.logosfooter  src/genweb.migrations \
-  src/genweb.packets  src/genweb.portlets     src/genweb.smartportlet \
-  src/genweb.stack    src/genweb.theme        src/genweb.upc/
+domain=genweb
 
-cd src/genweb.core/genweb/core/locales/
-../../../../../bin/i18ndude sync --pot genweb.pot ca/LC_MESSAGES/genweb.po
-../../../../../bin/i18ndude sync --pot genweb.pot es/LC_MESSAGES/genweb.po
-../../../../../bin/i18ndude sync --pot genweb.pot en/LC_MESSAGES/genweb.po
+../../../../../../bin/i18ndude rebuild-pot --pot $domain.pot --create $domain ../ \
+../../../../../genweb5.theme/src/genweb5/theme/
+
+../../../../../../bin/i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
