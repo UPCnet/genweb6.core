d8d7701be415e0d8a09bbde96429e6ba7bbd062d
Iago López <ilopezsmx2011@gmail.com>
2022-05-16 08:07:06 +0200

    Migrate genweb.upc.documentimage

---
+++ src/genweb6/core/content/document_image/document_image.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+
+from plone.app.contenttypes import _
+from plone.dexterity.content import Item
+from plone.namedfile import field as namedfile
+from plone.supermodel import model
+from zope import schema
+from zope.interface import implementer
+
+from genweb6.core import _ as _GwC
+
+
+class IDocumentImage(model.Schema):
+
+    image = namedfile.NamedBlobImage(
+        title=_(u"Lead Image"),
+        description=u"",
+        required=True,
+    )
+
+    not_show_image = schema.Bool(
+        title=_GwC(u"not_show_image"),
+        description=u"",
+        required=False,
+    )
+
+    image_caption = schema.TextLine(
+        title=_(u"Lead Image Caption"),
+        description=u"",
+        required=False,
+    )
+
+
+@implementer(IDocumentImage)
+class DocumentImage(Item):
+    pass
+
+
+class View(BrowserView):
+    pass
