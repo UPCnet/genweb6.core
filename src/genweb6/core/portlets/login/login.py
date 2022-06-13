b76d3b278845dc13e8b7da7dec6ee934fa7ea9af
Iago López <ilopezsmx2011@gmail.com>
2022-06-13 16:06:58 +0200

    Portlet login

---
+++ src/genweb6/core/portlets/login/login.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.portlets.portlets.login import Renderer as LoginRenderer
+
+from genweb6.core.browser.login import LoginUtils
+
+
+class gwLogin(LoginRenderer, LoginUtils):
+    """ The standard navigation portlet override 'old style'
+        as it doesn't allow to do it jbot way...
+    """
+    render = ViewPageTemplateFile("login.pt")
