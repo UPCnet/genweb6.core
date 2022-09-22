c6d37505533ba109dfd7399a43161802c86aba49
Iago López <ilopezsmx2011@gmail.com>
2022-09-22 10:42:54 +0200

    Solucionar bug carousel complejo

--- src/genweb6/core/portlets/multiviewcollection/templates/carousel_complex.js
+++ src/genweb6/core/portlets/multiviewcollection/templates/carousel_complex.js
@@ -1,14 +1,18 @@
-var items = document.querySelectorAll('.portlet-collection-carousel-complex .carousel .carousel-item');
+let items = document.querySelectorAll('.portlet-collection-carousel-complex .carousel .carousel-item');
 
 items.forEach((el) => {
+
   const minPerSlide = 4;
   let next = el.nextElementSibling;
+
   for (var i=1; i<minPerSlide; i++) {
     if (!next) {
       next = items[0];
     }
+
     let cloneChild = next.cloneNode(true);
     el.appendChild(cloneChild.children[0]);
     next = next.nextElementSibling;
   }
+
 });
