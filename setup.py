4c93268c4503c98d0144849281f576f910e0e674
Ivan Kosovtsev <ivan.kosovtsev@ithinkupc.com>
2023-04-28 13:58:39 +0200

    Configurar buildout del paqeute para CI/CD

--- setup.py
+++ setup.py
@@ -82,7 +82,8 @@ setup(
             # plone_coredev tests as of 2016-04-01.
             'plone.testing',
             'plone.app.contenttypes',
-            'plone.app.robotframework[debug]',
+            'mock'
+            #'plone.app.robotframework[debug]',
         ],
     },
     entry_points="""
