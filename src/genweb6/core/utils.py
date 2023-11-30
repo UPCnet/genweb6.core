3c85cb00eaf59b46a03b8ceec6925c157a3157dc
Pilar Marinas <pilar.marinas@upcnet.es>
2023-11-30 15:57:23 +0100

    purge cache varnish si esta configurado

--- src/genweb6/core/utils.py
+++ src/genweb6/core/utils.py
@@ -223,22 +223,24 @@ def purge_varnish_paths(self, paths):
     purgingSettings = registry.forInterface(ICachePurgingSettings)
     proxies = purgingSettings.cachingProxies
 
-    def purge(url):
-        status, xcache, xerror = purger.purgeSync(url)
-        log = url
-        if xcache:
-            log += " (X-Cache header: " + xcache + ")"
-        if xerror:
-            log += " -- " + xerror
-        if not str(status).startswith("2"):
-            log += " -- WARNING status " + str(status)
+    if proxies:
 
-    relativePaths = [x.decode("utf8") if isinstance(x, bytes) else x for x in paths]
-    rewriter = IPurgePathRewriter(self.request, None)
-    for relativePath in relativePaths:
-        rewrittenPaths = rewriter(relativePath) or []
-        for newURL in getURLsToPurge(rewrittenPaths[0], proxies):
-            purge(newURL)
+        def purge(url):
+            status, xcache, xerror = purger.purgeSync(url)
+            log = url
+            if xcache:
+                log += " (X-Cache header: " + xcache + ")"
+            if xerror:
+                log += " -- " + xerror
+            if not str(status).startswith("2"):
+                log += " -- WARNING status " + str(status)
+
+        relativePaths = [x.decode("utf8") if isinstance(x, bytes) else x for x in paths]
+        rewriter = IPurgePathRewriter(self.request, None)
+        for relativePath in relativePaths:
+            rewrittenPaths = rewriter(relativePath) or []
+            for newURL in getURLsToPurge(rewrittenPaths[0], proxies):
+                purge(newURL)
 
 # class GWConfig(BrowserView):
 
