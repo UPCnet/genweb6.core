4fe20641fef8390652ea96d6d21a6734622e9e2b
Iago López <ilopezsmx2011@gmail.com>
2023-12-12 10:41:06 +0100

    [ADD] No mostrar elementos excluidos de la navegación en colecciones

--- src/genweb6/core/patches.py
+++ src/genweb6/core/patches.py
@@ -1400,3 +1400,27 @@ def resultsFolder(self, **kwargs):
         return []
     results = listing(**kwargs)
     return results
+
+def resultsCollection(self, **kwargs):
+    """Return a content listing based result set with results from the
+    collection query.
+
+    :param **kwargs: Any keyword argument, which can be used for catalog
+                        queries.
+    :type  **kwargs: keyword argument
+
+    :returns: plone.app.contentlisting based result set.
+    :rtype: ``plone.app.contentlisting.interfaces.IContentListing`` based
+            sequence.
+    """
+    # Extra filter
+    contentFilter = dict(self.request.get("contentFilter", {}))
+    contentFilter.update(kwargs.get("contentFilter", {}))
+    contentFilter.update({'exclude_from_nav': False})  # Añadido
+    kwargs.setdefault("custom_query", contentFilter)
+    kwargs.setdefault("batch", True)
+    kwargs.setdefault("b_size", self.b_size)
+    kwargs.setdefault("b_start", self.b_start)
+
+    results = self.collection_behavior.results(**kwargs)
+    return results
