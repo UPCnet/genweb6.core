d36e66d90efc81501d0f9fc612159dc934ebe346
Pilar Marinas <pilar.marinas@upcnet.es>
2024-01-15 13:30:50 +0100

    Back to development: 1.27

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.core',
-    version='1.26',
+    version='1.27.dev0',
     description="Genweb 6 Core package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
