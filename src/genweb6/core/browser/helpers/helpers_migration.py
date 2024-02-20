2b05df7647dcd8379c2547fbc2a421c81e39726d
Pilar Marinas <pilar.marinas@upcnet.es>
2024-02-20 08:38:04 +0100

    [FIX] migrationfixtemplates add  div class=mceTmpl in templates

--- src/genweb6/core/browser/helpers/helpers_migration.py
+++ src/genweb6/core/browser/helpers/helpers_migration.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
+from plone import api
+from plone.app.textfield.value import RichTextValue
 
 from zope.interface import alsoProvides
 
@@ -44,6 +46,39 @@ Exemples:
                 msg += obj.absolute_url() + '\n'
                 if 'list' not in self.request.form:
                     obj.setLayout(target)
+
+        logger.info('- Ha finalitzat el procés.')
+        return msg + '\nHa finalitzat el procés.'
+
+class migrationfixtemplates(BrowserView):
+    """
+Aquesta vista afegeix a las plantilles <div class="mceTmpl"></div>
+"""
+
+    def __call__(self):
+        try:
+            from plone.protect.interfaces import IDisableCSRFProtection
+            alsoProvides(self.request, IDisableCSRFProtection)
+        except:
+            pass
+
+        portal = api.portal.get()
+        pc = api.portal.get_tool(name='portal_catalog')
+        plantilles = pc.searchResults(portal_type='Document',
+                                      path='/'.join(portal.getPhysicalPath()) + '/plantilles')
+
+        msg = ''
+        for brain in plantilles:
+            obj = brain.getObject()
+            if 'mceTmpl' not in obj.text.raw:
+                new_text = str('<div class="mceTmpl">' + obj.text.raw + '</div>')
+                obj.text = RichTextValue(new_text, 'text/plain', 'text/html')
+                obj.reindexObject()
+
+                logger.info('- Actualitzat: ' + obj.absolute_url())
+                msg += obj.absolute_url() + '\n'
+            else:
+                logger.info('- No cal actualitzar: ' + obj.absolute_url())
 
         logger.info('- Ha finalitzat el procés.')
         return msg + '\nHa finalitzat el procés.'
