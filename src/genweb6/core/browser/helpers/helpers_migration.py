3d8cf7e599942abf8a5817fd7aaf81d937cfb174
Pilar Marinas <pilar.marinas@upcnet.es>
2024-04-19 13:31:26 +0200

    Arreglar colecciones rotas criterios migrador

--- src/genweb6/core/browser/helpers/helpers_migration.py
+++ src/genweb6/core/browser/helpers/helpers_migration.py
@@ -6,6 +6,7 @@ from plone.app.textfield.value import RichTextValue
 from zope.interface import alsoProvides
 
 import logging
+import transaction
 
 logger = logging.getLogger(__name__)
 
@@ -82,3 +83,109 @@ Aquesta vista afegeix a las plantilles <div class="mceTmpl"></div>
 
         logger.info('- Ha finalitzat el procés.')
         return msg + '\nHa finalitzat el procés.'
+
+class fix_collection_migration(BrowserView):
+    """
+Arreglar colecciones rotas criterios migrador
+    """
+
+    def __call__(self):
+        try:
+            from plone.protect.interfaces import IDisableCSRFProtection
+            alsoProvides(self.request, IDisableCSRFProtection)
+        except:
+            pass
+
+        indexes_to_fix = [
+            u'portal_type',
+            u'review_state',
+            u'Creator'
+        ]
+
+        operator_mapping = {
+            "plone.app.querystring.operation.selection.is":
+            "plone.app.querystring.operation.selection.any",
+            # old -> new
+            "plone.app.querystring.operation.string.is":
+            "plone.app.querystring.operation.selection.any",
+        }
+
+        operator_mapping_or = {
+            # old -> new
+            u"plone.app.querystring.operation.selection.is":
+                u"plone.app.querystring.operation.selection.any",
+            u"plone.app.querystring.operation.string.is":
+                u"plone.app.querystring.operation.selection.any",
+        }
+
+        operator_mapping_and = {
+            # old -> new
+            u"plone.app.querystring.operation.selection.is":
+                u"plone.app.querystring.operation.selection.all",
+            u"plone.app.querystring.operation.string.is":
+                u"plone.app.querystring.operation.selection.all",
+        }
+
+        PORTAL_TYPE_MAPPING = {
+            "Topic": "Collection",
+            "Window": "Document",
+        }
+
+        REVIEW_STATE_MAPPING = {}
+
+        catalog = api.portal.get_tool('portal_catalog')
+        brains = catalog.unrestrictedSearchResults(
+            portal_type=['Collection']
+        )
+
+        for brain in brains:
+            item = brain.getObject()
+            query = item.query
+            fixed_query = []
+
+            for crit in query:
+                if crit["i"] == "portal_type" and len(crit["v"]) > 30:
+                    # Criterion is all types
+                    continue
+
+                if crit["o"].endswith("relativePath") and crit["v"] == "..":
+                    # relativePath no longer accepts ..
+                    crit["v"] = "..::1"
+
+                if crit["i"] in indexes_to_fix:
+                    for old_operator, new_operator in operator_mapping.items():
+                        if crit["o"] == old_operator:
+                            crit["o"] = new_operator
+
+                if crit["o"] == "plone.app.querystring.operation.selection.all" and len(crit["v"]) == 1:
+                    # all operator with one value is equivalent to any
+                    crit["o"] = "plone.app.querystring.operation.selection.any"
+
+                if crit["i"] == "portal_type":
+                    # Some types may have changed their names
+                    fixed_types = []
+                    for portal_type in crit["v"]:
+                        fixed_type = PORTAL_TYPE_MAPPING.get(portal_type, portal_type)
+                        fixed_types.append(fixed_type)
+                    crit["v"] = list(set(fixed_types))
+
+                if crit["i"] == "review_state":
+                    # Review states may have changed their names
+                    fixed_states = []
+                    for review_state in crit["v"]:
+                        fixed_state = REVIEW_STATE_MAPPING.get(review_state, review_state)
+                        fixed_states.append(fixed_state)
+                    crit["v"] = list(set(fixed_states))
+
+                if crit["o"] == "plone.app.querystring.operation.string.currentUser":
+                    crit["v"] = ""
+
+                fixed_query.append(crit)
+
+            logger.info('- Actualitzada Collection: ' + item.absolute_url())
+            logger.info('- Collection query old: ' + str(query))
+            logger.info('- Collection query new: ' + str(fixed_query))
+            item.reindexObject()
+
+        transaction.commit()
+        return 'OK'
