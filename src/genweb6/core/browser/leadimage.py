94b126e12b03965d9884c9b1e0a1c073ed628018
Iago López <iago.lopez@upcnet.es>
2025-05-07 17:00:31 +0200

    [FIX] show lead image

--- src/genweb6/core/browser/leadimage.py
+++ src/genweb6/core/browser/leadimage.py
@@ -1,14 +1,13 @@
+# -*- coding: utf-8 -*-
 from plone.app.contenttypes.behaviors.viewlets import LeadImageViewlet
-from plone.app.contenttypes.behaviors.leadimage import ILeadImage
-from zope.interface import alsoProvides
+from plone.event.interfaces import IOccurrence
+
 
 class CustomLeadImageViewlet(LeadImageViewlet):
-    def update(self):
-        # Accede al objeto original si es una ocurrencia
-        real_context = getattr(self.context, '__parent__', self.context)
 
-        # Solo usa el objeto real si tiene la interfaz de lead image
-        if ILeadImage.providedBy(real_context):
-            self.context = real_context
+    def update(self):
+        # Si es una Occurrence se va al contexto del Evento
+        if IOccurrence.providedBy(self.context):
+            self.context = getattr(self.context, '__parent__', self.context)
 
         super().update()
