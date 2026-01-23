f91526794445adddc5196471b80c470667fbcfc6
Clàudia Andreu <claudia.andreu@ithinkupc.com>
2026-01-23 12:13:00 +0100

    [ADD] checkbox to disable news image

--- src/genweb6/core/behaviors/event_image.py
+++ src/genweb6/core/behaviors/event_image.py
@@ -39,3 +39,34 @@ class EventImage(object):
 
     not_show_image = property(_get_not_show_image, _set_not_show_image)
 
+
+class INewsImage(ILeadImage):
+    """Extend ILeadImage to add not_show_image field for news
+    """
+
+    not_show_image = schema.Bool(
+        title=_(u"not_show_image"),
+        description=_(u"No mostreu la imatge a la pàgina, però sí que ho feu als portlets i a les vistes."),
+        required=False,
+        default=False,
+    )
+
+alsoProvides(INewsImage, IFormFieldProvider)
+
+
+@implementer(INewsImage)
+class NewsImage(object):
+    """Adapter for INewsImage behavior
+    """
+
+    def __init__(self, context):
+        self.context = context
+
+    def _get_not_show_image(self):
+        return getattr(self.context, 'not_show_image', False)
+
+    def _set_not_show_image(self, value):
+        self.context.not_show_image = value
+
+    not_show_image = property(_get_not_show_image, _set_not_show_image)
+
--- src/genweb6/core/browser/leadimage.py
+++ src/genweb6/core/browser/leadimage.py
@@ -2,10 +2,10 @@
 from plone.app.contenttypes.behaviors.viewlets import LeadImageViewlet
 from plone.event.interfaces import IOccurrence
 from plone.app.contenttypes.behaviors.leadimage import ILeadImage
-from plone.app.contenttypes.interfaces import IEvent
-from genweb6.core.behaviors.event_image import IEventImage
+from plone.app.contenttypes.interfaces import IEvent, INewsItem
+from genweb6.core.behaviors.event_image import IEventImage, INewsImage
 from zope.interface import alsoProvides
-from zope.component import queryAdapter
+from zope.component import queryAdapter, getAdapter
 
 
 class CustomLeadImageViewlet(LeadImageViewlet):
@@ -23,13 +23,23 @@ class CustomLeadImageViewlet(LeadImageViewlet):
         # Si es un Event, verificar si tiene not_show_image activado
         # Si está activado, ocultar la imagen en la página (pero mostrarla en portlets)
         if IEvent.providedBy(self.context):
-            # Intentar obtener el valor del campo usando el adapter del behavior
             try:
-                from zope.component import getAdapter
                 event_image = getAdapter(self.context, IEventImage)
                 not_show_image = event_image.not_show_image
             except:
-                # Si no se puede obtener el adapter, leer directamente del contexto
+                not_show_image = getattr(self.context, 'not_show_image', False)
+            
+            if not_show_image:
+                self.available = False
+                return
+        
+        # Si es un News Item, verificar si tiene not_show_image activado
+        # Si está activado, ocultar la imagen en la página (pero mostrarla en portlets)
+        if INewsItem.providedBy(self.context):
+            try:
+                news_image = getAdapter(self.context, INewsImage)
+                not_show_image = news_image.not_show_image
+            except:
                 not_show_image = getattr(self.context, 'not_show_image', False)
             
             if not_show_image:
