3e737ecef808559559585074912e313565423b4e
Clàudia Andreu <claudia.andreu@ithinkupc.com>
2026-01-29 09:50:38 +0100

    [UPD] unify event and news behavior

--- src/genweb6/core/behaviors/event_image.py
+++ src/genweb6/core/behaviors/event_image.py
@@ -9,8 +9,8 @@ from zope.interface import implementer
 from genweb6.core import _
 
 
-class IEventImage(ILeadImage):
-    """Extend ILeadImage to add not_show_image field for events
+class IContentImage(ILeadImage):
+    """Extend ILeadImage to add not_show_image field for any content type
     """
 
     not_show_image = schema.Bool(
@@ -20,43 +20,12 @@ class IEventImage(ILeadImage):
         default=False,
     )
 
-alsoProvides(IEventImage, IFormFieldProvider)
+alsoProvides(IContentImage, IFormFieldProvider)
 
 
-@implementer(IEventImage)
-class EventImage(object):
-    """Adapter for IEventImage behavior
-    """
-
-    def __init__(self, context):
-        self.context = context
-
-    def _get_not_show_image(self):
-        return getattr(self.context, 'not_show_image', False)
-
-    def _set_not_show_image(self, value):
-        self.context.not_show_image = value
-
-    not_show_image = property(_get_not_show_image, _set_not_show_image)
-
-
-class INewsImage(ILeadImage):
-    """Extend ILeadImage to add not_show_image field for news
-    """
-
-    not_show_image = schema.Bool(
-        title=_(u"not_show_image"),
-        description=_(u"No mostreu la imatge a la pàgina, però sí que ho feu als portlets i a les vistes."),
-        required=False,
-        default=False,
-    )
-
-alsoProvides(INewsImage, IFormFieldProvider)
-
-
-@implementer(INewsImage)
-class NewsImage(object):
-    """Adapter for INewsImage behavior
+@implementer(IContentImage)
+class ContentImage(object):
+    """Adapter for IContentImage behavior
     """
 
     def __init__(self, context):
--- src/genweb6/core/browser/leadimage.py
+++ src/genweb6/core/browser/leadimage.py
@@ -2,10 +2,8 @@
 from plone.app.contenttypes.behaviors.viewlets import LeadImageViewlet
 from plone.event.interfaces import IOccurrence
 from plone.app.contenttypes.behaviors.leadimage import ILeadImage
-from plone.app.contenttypes.interfaces import IEvent, INewsItem
-from genweb6.core.behaviors.event_image import IEventImage, INewsImage
-from zope.interface import alsoProvides
-from zope.component import queryAdapter, getAdapter
+from genweb6.core.behaviors.event_image import IContentImage
+from zope.component import getAdapter
 
 
 class CustomLeadImageViewlet(LeadImageViewlet):
@@ -20,31 +18,17 @@ class CustomLeadImageViewlet(LeadImageViewlet):
             self.available = False
             return
         
-        # Si es un Event, verificar si tiene not_show_image activado
+        # Verificar si tiene not_show_image activado
         # Si está activado, ocultar la imagen en la página (pero mostrarla en portlets)
-        if IEvent.providedBy(self.context):
-            try:
-                event_image = getAdapter(self.context, IEventImage)
-                not_show_image = event_image.not_show_image
-            except:
-                not_show_image = getattr(self.context, 'not_show_image', False)
-            
-            if not_show_image:
-                self.available = False
-                return
+        try:
+            content_image = getAdapter(self.context, IContentImage)
+            not_show_image = content_image.not_show_image
+        except:
+            not_show_image = getattr(self.context, 'not_show_image', False)
         
-        # Si es un News Item, verificar si tiene not_show_image activado
-        # Si está activado, ocultar la imagen en la página (pero mostrarla en portlets)
-        if INewsItem.providedBy(self.context):
-            try:
-                news_image = getAdapter(self.context, INewsImage)
-                not_show_image = news_image.not_show_image
-            except:
-                not_show_image = getattr(self.context, 'not_show_image', False)
-            
-            if not_show_image:
-                self.available = False
-                return
+        if not_show_image:
+            self.available = False
+            return
         
         self.available = True
 
